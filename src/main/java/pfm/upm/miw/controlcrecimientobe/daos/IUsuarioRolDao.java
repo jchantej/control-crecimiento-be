b13b537de974fa745f822ba28b46373c07663568
Juan Jimenez <jchantej@gmail.com>
2018-05-30 12:12:58 +0200

    issue#7 Creacion de los Doas usuario y rol

---
+++ src/main/java/pfm/upm/miw/controlcrecimientobe/daos/IUsuarioRolDao.java
@@ -0,0 +1,11 @@
+package pfm.upm.miw.controlcrecimientobe.daos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pfm.upm.miw.controlcrecimientobe.entidades.UsuarioRol;
+
+
+
+public interface IUsuarioRolDao  extends JpaRepository<UsuarioRol, Integer>{
+    
+
+}
